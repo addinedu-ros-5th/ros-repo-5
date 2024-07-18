3e58471a47a160f87b346be9ab2aa655d4211849
Shoons23 <jaehoon9723@gmail.com>
2024-07-18 13:33:10 +0900

    modify : code : RFID_test

--- RH_lot/example/SPI_test/SPI_test.ino
+++ RH_lot/example/SPI_test/SPI_test.ino
@@ -1,66 +1,44 @@
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-rfid-nfc
+ */
+
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define SS_PIN 5      // SDA(SS) - GPIO5
-#define SCK_PIN 18    // SCK - GPIO18
-#define MOSI_PIN 23   // MOSI - GPIO23
-#define MISO_PIN 19   // MISO - GPIO19
+#define SS_PIN  5  // ESP32 pin GPIO5 
+#define RST_PIN 27 // ESP32 pin GPIO27 
 
-// GND - GND 
-#define RST_PIN 21 // RST - GPIO21
-// VCC - 3V3
-MFRC522 mfrc522(SS_PIN, RST_PIN);  
+MFRC522 rfid(SS_PIN, RST_PIN);
 
 void setup() {
-  Serial.begin(115200);
-  SPI.begin();
-  pinMode(SS_PIN, OUTPUT);
-  digitalWrite(SS_PIN, HIGH);
+  Serial.begin(9600);
+  SPI.begin(); // init SPI bus
+  rfid.PCD_Init(); // init MFRC522
 
-  mfrc522.PCD_Init();
-  
-  #
-  byte v = mfrc522.PCD_ReadRegister(MFRC522::VersionReg);
-  Serial.print("MFRC522 Software Version: 0x");
-  Serial.println(v, HEX);
-  if (v == 0x91 || v == 0x92)
-    Serial.println("MFRC522 detected.");
-  else
-    Serial.println("MFRC522 not detected. Check wiring.");
+  Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader");
 }
 
 void loop() {
-  digitalWrite(SS_PIN, LOW);
-  SPI.transfer(0x55);  // 임의의 데이터 전송
-  digitalWrite(SS_PIN, HIGH);
-  Serial.println("SPI transfer completed");
-  delay(1000);
+  if (rfid.PICC_IsNewCardPresent()) { // new tag is available
+    if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+      Serial.print("RFID/NFC Tag Type: ");
+      Serial.println(rfid.PICC_GetTypeName(piccType));
 
-  Serial.println("Scanning...");
-  
-  if (!mfrc522.PICC_IsNewCardPresent()) {
-    Serial.println("No new card");
-    delay(500);
-    return;
+      // print UID in Serial Monitor in the hex format
+      Serial.print("UID:");
+      for (int i = 0; i < rfid.uid.size; i++) {
+        Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(rfid.uid.uidByte[i], HEX);
+      }
+      Serial.println();
+
+      rfid.PICC_HaltA(); // halt PICC
+      rfid.PCD_StopCrypto1(); // stop encryption on PCD
+    }
   }
-  
-  if (!mfrc522.PICC_ReadCardSerial()) {
-    Serial.println("Failed to read card");
-    delay(500);
-    return;
-  }
-  
-  Serial.println("Card detected!");
-  // 카드 정보 출력
-  Serial.print("Card UID:");
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-  }
-  Serial.println();
-  
-  mfrc522.PICC_HaltA();
-  mfrc522.PCD_StopCrypto1();
-  
-  delay(1000);
 }
