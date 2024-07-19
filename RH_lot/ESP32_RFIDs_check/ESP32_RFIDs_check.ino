2c7cfe9774eda28c3d096ebcafa2ee8400e6fcb1
jinsa <jaehoon9723@gmail.com>
2024-07-19 12:19:50 +0900

    update : code : TCP/IP RFID test

--- RH_lot/ESP32_RFIDs_check/ESP32_RFIDs_check.ino
+++ RH_lot/ESP32_RFIDs_check/ESP32_RFIDs_check.ino
@@ -1,6 +1,7 @@
+#include <WiFi.h>
 #include <SPI.h>
 #include <MFRC522.h>
-
+#include "config.h" 
 // RFID 1
 #define SS_PIN 5      // SDA(SS) - GPIO5
 #define SCK_PIN 18    // SCK - GPIO18
@@ -10,9 +11,6 @@
 
 // RFID 2
 #define SS_PIN2 26    // SDA(SS) - GPIO26
-//#define SCK_PIN 18    // SCK - GPIO18
-//#define MOSI_PIN 23   // MOSI - GPIO23
-//#define MISO_PIN 19   // MISO - GPIO19
 #define RST_PIN2 13   // RST - GPIO21
 
 #define BUILTIN_LED_PIN 2 // BUILTIN_LED_PIN
@@ -21,10 +19,21 @@ byte targetUID[4] = {0xC3, 0x49, 0x6F, 0x96};
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // MFRC522 RFID module
 MFRC522 mfrc522_2(SS_PIN2, RST_PIN2);   // MFRC522 RFID module
 
+WiFiClient client;
+
 void setup() {
   Serial.begin(115200);
   while (!Serial);
   Serial.println("Serial communication started");
+  
+  // WiFi 연결
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
   SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, SS_PIN);  // SCK, MISO, MOSI, SS for the first reader
   SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, SS_PIN2);  // SCK, MISO, MOSI, SS for the second reader
   pinMode(SS_PIN, OUTPUT);
@@ -45,17 +54,15 @@ void loop() {
 }
 
 void checkRFID(MFRC522 &rfid) {
-  // RFID scan
   if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
     Serial.println("Card detected!");
-    // Output card information
-    Serial.print("Card UID:");
+    String uidString = "";
     for (byte i = 0; i < rfid.uid.size; i++) {
-      Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
-      Serial.print(rfid.uid.uidByte[i], HEX);
+      uidString += String(rfid.uid.uidByte[i] < 0x10 ? "0" : "");
+      uidString += String(rfid.uid.uidByte[i], HEX);
     }
-    Serial.println();
-    // Compare the scanned UID with the target UID
+    Serial.println("Card UID: " + uidString);
+    
     bool isMatch = true;
     for (byte i = 0; i < 4; i++) {
       if (rfid.uid.uidByte[i] != targetUID[i]) {
@@ -63,20 +70,33 @@ void checkRFID(MFRC522 &rfid) {
         break;
       }
     }
+    
     if (isMatch) {
-      // Turn on LED if UID matches
       digitalWrite(BUILTIN_LED_PIN, HIGH);
       Serial.println("Matching card detected. LED turned ON.");
-      delay(3000);  // Keep the LED on for 3 seconds
+      sendToServer("MATCH:" + uidString);
+      delay(3000);
       digitalWrite(BUILTIN_LED_PIN, LOW);
       Serial.println("LED turned OFF after 3 seconds.");
     } else {
-      // Turn off LED if UID does not match
       digitalWrite(BUILTIN_LED_PIN, LOW);
       Serial.println("Non-matching card detected. LED turned OFF.");
+      sendToServer("NOMATCH:" + uidString);
     }
-    Serial.println("Scanned!");
+    
     rfid.PICC_HaltA();
     rfid.PCD_StopCrypto1();
+  }
+}
+
+void sendToServer(String message) {
+  if (client.connect(server, port)) {
+    Serial.println("Connected to server");
+    client.println(message);
+    Serial.println("Message sent: " + message);
+    client.stop();
+    Serial.println("Disconnected from server");
+  } else {
+    Serial.println("Connection to server failed");
   }
 }
---
+++ RH_lot/ESP32_RFIDs_check/config.h
@@ -0,0 +1,17 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WiFi info
+const char* ssid = "addinedu_class_2 (2.4G)";
+const char* password = "addinedu1";
+const char* ssid2 = "your_ssid2";
+const char* password2 = "your_password2";
+
+// Server info
+const char* server = "192.168.0.37";
+const int port = 12346;
+const char* server2 = "192.168.0.38";
+const int port2 = 12347;
+
+#endif
