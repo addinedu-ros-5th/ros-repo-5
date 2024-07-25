19e1cca54dbfaf34d8af85946b0635b594d77be6
1yohanyo1 <yohangim119@gmail.com>
2024-07-25 14:25:59 +0900

    update : code : update code

--- Main_server/forward_command/forward_command/forward_node.py
+++ Main_server/forward_command/forward_command/forward_node.py
@@ -2,11 +2,12 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist, Pose
-from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseWithCovarianceStamped
 import math
 import json
 import os
+import subprocess
+from std_srvs.srv import Empty
 
 class PIDController:
     def __init__(self, kp, ki, kd):
@@ -33,7 +34,6 @@ class WaypointNavigator(Node):
             10)
         self.publisher_ = self.create_publisher(Twist, '/base_controller/cmd_vel_unstamped', 10)
         self.amcl_subscription = self.create_subscription(PoseWithCovarianceStamped, '/amcl_pose', self.amcl_callback, 10)
-        
         self.send_park_num_publisher = self.create_publisher(String, '/send_park_num', 10)
 
         self.amcl_pose = Pose()  # Initialize amcl_pose
@@ -53,7 +53,21 @@ class WaypointNavigator(Node):
         self.last_time = self.get_clock().now()
 
         self.current_route = ""
-        self.tg1_published = False  # TG1 메시지가 한 번 발행되었는지 확인하는 플래그 변수
+        self.tg1_published = False  # 발행 플래그 추가
+        self.post_action_performed = False
+
+        self.srv = self.create_service(Empty, 'reset_node', self.reset_node_callback)
+
+        # GOGO 신호와 대응하는 SA 신호 딕셔너리
+        self.gogo_signal_to_park_num = {
+            'GOGO': 'SA2TG1',
+            'GOGO1': 'SA2TG2',
+            'GOGO2': 'SB2TG1',
+            'GOGO3': 'SB2TG2',
+            'RG1TF1': 'SG1TF1',
+            'RG1TO1': 'SG1TO1',
+            'RF1TO1': 'SF1TO1'
+        }
 
     def load_waypoints(self):
         waypoints_file = '/home/ros2/pinkbot/src/pinklab_minibot_robot/forward_command/forward_command/waypoints.json'
@@ -77,9 +91,21 @@ class WaypointNavigator(Node):
             self.reached_waypoint = False
             self.navigation_active = True
             self.current_route = msg.data
-            self.tg1_published = False  # 새로운 작업이 시작될 때 플래그 초기화
+            self.tg1_published = False  # Reset publish flag
+            self.post_action_performed = False
+        elif msg.data in self.gogo_signal_to_park_num:
+            # GOGO 시그널 처리
+            self.get_logger().info(f'Starting waypoint navigation for GOGO route {msg.data}')
+            waypoint_indices = list(range(1, 5))  # 예시: GOGO 시그널에 대해 기본 waypoints 인덱스 설정
+            self.current_waypoints = [self.waypoints_dict[0][i - 1] for i in waypoint_indices]
+            self.current_waypoint_index = 0
+            self.reached_waypoint = False
+            self.navigation_active = True
+            self.current_route = msg.data
+            self.tg1_published = False  # Reset publish flag for GOGO signals
+            self.post_action_performed = False
         else:
-            self.get_logger().info(f'Invalid route: {msg.data}')
+            self.get_logger().info(f'Invalid route or signal: {msg.data}')
 
     def amcl_callback(self, msg):
         self.amcl_pose = msg.pose.pose
@@ -90,35 +116,26 @@ class WaypointNavigator(Node):
     def navigate_to_waypoints(self):
         if not self.navigation_active:
             return
-        
+
         if self.current_waypoint_index >= len(self.current_waypoints):
             self.get_logger().info('All waypoints reached')
             self.navigation_active = False
 
-            # 현재 경로에 따라 send_park_num 토픽에 메시지 발행
+            # 현재 경로에 따라 후속 작업 처리
             if self.current_route in ['RA2TG1', 'RB2TG1', 'RA2TG2', 'RB2TG2']:
+                if not self.post_action_performed:
+                    self.perform_post_navigation_action()
+            elif self.current_route in self.gogo_signal_to_park_num:
                 if not self.tg1_published:
-                    new_msg = String()
-                    if self.current_route == 'RA2TG1':
-                        new_msg.data = 'SA2TG1'
-                    elif self.current_route == 'RB2TG1':
-                        new_msg.data = 'SB2TG1'
-                    elif self.current_route == 'RA2TG2':
-                        new_msg.data = 'SA2TG2'
-                    elif self.current_route == 'RB2TG2':
-                        new_msg.data = 'SB2TG2'
-                    self.send_park_num_publisher.publish(new_msg)
+                    park_num_signal = self.gogo_signal_to_park_num[self.current_route]
+                    self.get_logger().info(f'Publishing {park_num_signal} to /send_park_num')
+                    msg = String()
+                    msg.data = park_num_signal
+                    self.send_park_num_publisher.publish(msg)
                     self.tg1_published = True  # 한 번만 발행되도록 설정
-            elif self.current_route in ['RG1TO1', 'RG1TF1']:
-                new_msg = String()
-                if self.current_route == 'RG1TO1':
-                    new_msg.data = 'SG1TO1'
-                elif self.current_route == 'RG1TF1':
-                    new_msg.data = 'SG1TF1'
-                self.send_park_num_publisher.publish(new_msg)
-
             return
 
+        self.get_logger().info('Navigating to waypoint')
         goal = self.current_waypoints[self.current_waypoint_index]
         goal_x, goal_y = goal['x'], goal['y']
         current_pose = self.get_current_pose()
@@ -130,14 +147,15 @@ class WaypointNavigator(Node):
         dt = (current_time - self.last_time).nanoseconds / 1e9
         self.last_time = current_time
 
-        if distance < self.tolerance and not self.reached_waypoint:
-            self.get_logger().info(f'Waypoint {self.current_waypoint_index} reached')
-            self.reached_waypoint = True
+        if distance < self.tolerance:
+            if not self.reached_waypoint:
+                self.get_logger().info(f'Waypoint {self.current_waypoint_index} reached')
+                self.reached_waypoint = True
 
-        if self.reached_waypoint:
-            self.get_logger().info(f'Orientation at waypoint {self.current_waypoint_index} achieved')
-            self.current_waypoint_index += 1
-            self.reached_waypoint = False
+            if self.reached_waypoint:
+                self.get_logger().info(f'Orientation at waypoint {self.current_waypoint_index} achieved')
+                self.current_waypoint_index += 1
+                self.reached_waypoint = False
         else:
             angle_to_goal = math.atan2(goal_y - current_y, goal_x - current_x)
             current_yaw = self.get_yaw_from_quaternion(current_pose.orientation)
@@ -148,39 +166,77 @@ class WaypointNavigator(Node):
                 cmd.angular.z = self.angular_pid.compute(angle_diff, 0, dt)
             else:
                 cmd.linear.x = self.linear_pid.compute(distance, 0, dt)
-            cmd.angular.z = self.angular_pid.compute(angle_diff, 0, dt)
             self.publisher_.publish(cmd)
 
-    def start_navigation(self, route):
-        self.get_logger().info(f'Starting waypoint navigation for route {route}')
-        _, routes = self.waypoints_dict
-        waypoint_indices = routes[route]
-        self.current_waypoints = [self.waypoints_dict[0][i - 1] for i in waypoint_indices]
-        self.current_waypoint_index = 0
-        self.reached_waypoint = False
-        self.navigation_active = True
-        self.current_route = route
+    def perform_post_navigation_action(self):
+        # 제자리 회전 및 후진
+        self.get_logger().info('Performing post-navigation rotation and backward movement')
+        self.rotate_for_time(2.1)  # 90도 회전
+        rclpy.spin_once(self, timeout_sec=2)
+        self.move_backward_for_time(5)  # 5초간 후진
+
+        self.get_logger().info('Post-navigation action completed')
+        self.post_action_performed = True
+        self.reset_state()
+
+        # 노드 재시작
+        self.restart_node()
+
+    def rotate_for_time(self, angle):
+        cmd = Twist()
+        cmd.angular.z = 0.5  # 적절한 회전 속도로 설정
+        duration = angle / 0.5
+        self.publish_command_for_duration(cmd, duration)
+
+    def move_backward_for_time(self, duration):
+        cmd = Twist()
+        cmd.linear.x = -0.1  # 적절한 후진 속도로 설정
+        self.publish_command_for_duration(cmd, duration)
+
+    def publish_command_for_duration(self, cmd, duration):
+        start_time = self.get_clock().now()
+        while (self.get_clock().now() - start_time).nanoseconds / 1e9 < duration:
+            self.publisher_.publish(cmd)
+            rclpy.spin_once(self, timeout_sec=0.1)
+            self.get_logger().info('stop0')
+        self.publisher_.publish(Twist())  # 멈춤 명령 발행
+        self.get_logger().info('stop1')
 
     def get_yaw_from_quaternion(self, orientation):
         siny_cosp = 2 * (orientation.w * orientation.z + orientation.x * orientation.y)
         cosy_cosp = 1 - 2 * (orientation.y * orientation.y + orientation.z * orientation.z)
-        yaw = math.atan2(siny_cosp, cosy_cosp)
-        return yaw
+        return math.atan2(siny_cosp, cosy_cosp)
 
     def normalize_angle(self, angle):
-        while angle > math.pi:
-            angle -= 2 * math.pi
-        while angle < -math.pi:
-            angle += 2 * math.pi
-        return angle
+        return math.atan2(math.sin(angle), math.cos(angle))
+
+    def reset_state(self):
+        self.reached_waypoint = False
+        self.navigation_active = False
+        self.tg1_published = False
+        self.post_action_performed = False
+        self.current_waypoints = []
+        self.current_waypoint_index = 0
+        self.current_route = ""
+        self.get_logger().info('State has been reset, ready for new commands.')
+
+    def restart_node(self):
+        self.get_logger().info('Restarting node...')
+        subprocess.Popen(['ros2', 'run', 'forward_command', 'forward_node'])
+        self.get_logger().info('Node restart command issued')
+
+    def reset_node_callback(self, request, response):
+        self.get_logger().info('Received reset_node request. Restarting node...')
+        self.reset_state()
+        subprocess.Popen(['ros2', 'run', 'forward_command', 'forward_node'])
+        return response
 
 def main(args=None):
     rclpy.init(args=args)
-    waypoint_navigator = WaypointNavigator()
-    rclpy.spin(waypoint_navigator)
-    waypoint_navigator.destroy_node()
+    node = WaypointNavigator()
+    rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
-
